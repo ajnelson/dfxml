d217bff8de470cfe6bc7905a12d390a33d470955
Alex Nelson <ajnelson@cs.ucsc.edu>
2013-02-22 17:23:13 -0800

    Add more tests

--- tests/test_idifference.sh
+++ tests/test_idifference.sh
@@ -7,6 +7,8 @@ if [ $# -ne 0 ]; then
   exit 1
 fi
 
+trap "set +x; echo Test failed. >&2" EXIT
+
 set -e
 
 TESTSCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -21,23 +23,38 @@ assert_equal() {
   test `grep "$1" ${IDIFFERENCE_OUTPUT} | wc -l` -eq $2
 }
 
-assert_equal '\bNO_CHANGE\b' 0
+set -x
 
-#      <filename>CHANGE___content_and_mtime</filename>
-#      <filename>CHANGE___erased</filename>
+#idifference is known to pass these tests.
+assert_equal '\bNO_CHANGE\b' 0
+assert_equal '^-.*\bCHANGE___erased\b' 1
+assert_equal '^r.*\bCHANGE___renamed\b.*\b_CHANGE___renamed\b' 1
+assert_equal '^~.*\bCHANGE___content_and_mtime\b.*SHA1 changed' 1
+assert_equal '^~.*\bCHANGE___content_and_mtime\b.*mtime changed' 1
+assert_equal '^+.*\bCHANGE___new_file\b' 1
+#TODO Decide if it's "Correct" for an explicit time zone change to be registered as a difference.  Or, it might be worth just adding another flag to idifference.py to account for the user's decision.
+assert_equal '^~.*\bCHANGE___timestamp_changes_format_only\b.*mtime changed' 1
+assert_equal '^-.*\bCHANGE___unallocated\b' 1
+
+#(Test code templates)
+#assert_equal '^+.*\b\b' 1
+#assert_equal '^-.*\b\b' 1
+#assert_equal '^r.*\b\b.*\b\b' 1
+#assert_equal '^~.*\b\b.*' 1
+
+#TODO Write sufficient tests for these files
 #      <filename>CHANGE___erased___replaced_by_other_partition_file</filename>
 #      <filename>CHANGE___erased___replaced_by_sibling</filename><!--CHANGE: Inode remains the same, checksum does not.-->
 #      <filename>CHANGE___moved_to_erased_P1G_file</filename>
 #      <filename>CHANGE___move_from_P1G_to_P2G</filename>
-#      <filename>CHANGE___move_from_P1M_to_P3G___change_content___change_mtime</filename>
 #      <filename>_CHANGE___move_from_P1M_to_P3G___change_name</filename>
+#      <filename>CHANGE___move_from_P1M_to_P3G___change_content___change_mtime</filename>
 #      <filename>CHANGE___move_from_P1M_to_P3G___change_name</filename>
 #      <filename>CHANGE___move_from_P1M_to_P3G</filename>
-#      <filename>CHANGE___new_file</filename>
-#      <filename>_CHANGE___renamed</filename>
-#      <filename>CHANGE___renamed</filename>
 #      <filename>CHANGE___renamed_to_erased_sibling___change_checksum_and_mtime</filename>
-#      <filename>CHANGE___timestamp_changes_format_only</filename>
-#      <filename>CHANGE___unallocated</filename>
+
+set +x
+
+trap - EXIT
 
 echo "Tests successful."
